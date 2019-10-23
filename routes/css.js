8c3919e6b96d904e249f6baebd4423cb8d497a2c
eankeen <24364012+eankeen@users.noreply.github.com>
2019-10-22 18:54:20 -0700

    feat: add friendlier homepage with spectre.css

--- routes/css.js
+++ routes/css.js
@@ -6,4 +6,8 @@ router.get('/spectre.min.css', (req, res) => {
   res.sendFile(path.join(__dirname, '../node_modules/spectre.css/dist/spectre.min.css'));
 });
 
+router.get('/spectre-exp.min.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../node_modules/spectre.css/dist/spectre-exp.min.css'));
+});
+
 export default router;
