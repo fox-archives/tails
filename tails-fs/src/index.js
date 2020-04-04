0d630468a5b3a5a027bff1e9829b8837d4bfe780
eankeen <24364012+eankeen@users.noreply.github.com>
2020-04-03 20:42:03 -0700

    fix: not enabling es6 and ecma2017 globals in linting config

--- tails-fs/src/index.js
+++ tails-fs/src/index.js
@@ -1,6 +1,8 @@
 import './core/init'
+import * as TAILS_ERROR from './errors'
 
 export { PhysicalNamespace } from './namespace'
 export { PhysicalProject } from './project'
 export { Config, TAILS_CONFIG_FILE } from './config'
-export * as TAILS_ERROR from './errors'
+// export * as TAILS_ERROR from './errors' // linting errors
+export { TAILS_ERROR }
